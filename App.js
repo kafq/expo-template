592d1b39337012a5a8a402aab92d9cc65744375c
Denis Bakhmutov <dbak@FL430.local>
2018-12-14 13:04:34 +0200

    Set App.js to export all from App.tsx, allow network inspection

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import { Text } from 'react-native'
+/** 
+ *  Expo still requires presence of App.js file,
+ *  here it just exports the .tsx app
+ */
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <Text>Hello world</Text>
-    );
-  }
+import App from './src/App';
+
+// Allows to inspect Network requests
+
+global.XMLHttpRequest = global.originalXMLHttpRequest
+  ? global.originalXMLHttpRequest
+  : global.XMLHttpRequest
+global.FormData = global.originalFormData
+  ? global.originalFormData
+  : global.FormData
+
+fetch
+
+if (window.__FETCH_SUPPORT__) {
+  // it's RNDebugger only to have
+  window.__FETCH_SUPPORT__.blob = false
+} else {
+  global.Blob = global.originalBlob ? global.originalBlob : global.Blob
+  global.FileReader = global.originalFileReader
+    ? global.originalFileReader
+    : global.FileReader
 }
+
+export default App
